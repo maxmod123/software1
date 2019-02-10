92e881c2138f520f6b05e28351e242fc967c0ab1
kareemabusoud <kareem_maxlove@hotmail.com>
2019-02-10 15:02:13 +0200

    hi

---
+++ src/hw/test.java
@@ -0,0 +1,17 @@
+package hw;
+
+public class test {
+	int x1,y1 ;
+ test(int x , int y){
+ x1 =x ; 
+ y1 = y ;
+}
+ public int add (int x, int y) {
+	  int z =x+y ; 
+	  return z  ; 
+ }
+
+public void main () {
+	
+	}
+}
